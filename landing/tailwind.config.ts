fbe6d95ae9eb51bd697fdab40400e9ccaf747e2e
Kevandrew <143258544+Kevandrew@users.noreply.github.com>
2025-02-12 04:28:12 +0800

    Better landing

---
+++ landing/tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  darkMode: "class", // Change from "media" to "class"
+
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
